c315a96ca103e96aad027b214d0e4e3d51d25990
A-Kah <p2621987@my365.dmu.ac.uk>
2025-02-04 10:57:00 +0000

    added componenents directory, and greetings componenent

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Greeting from './components/greetings'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,7 +10,11 @@ function App() {
     <>
       <div>
        Hello World
+       <Greeting />
+      
       </div>
+      
+      
     </>
   )
 }
